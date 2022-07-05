4775da399ca22e2e0171969f2a4eabc3fc0d7229
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-07-05 07:03:58 +0200

    init

--- src/main/webapp/app/entities/pomoc/list/pomoc.component.ts
+++ src/main/webapp/app/entities/pomoc/list/pomoc.component.ts
@@ -1,48 +1,39 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { HttpResponse } from '@angular/common/http';
-
-import { IPomoc } from '../pomoc.model';
-import { PomocService } from '../service/pomoc.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'jhi-pomoc',
   templateUrl: './pomoc.component.html',
 })
 export class PomocComponent {
-  @ViewChild('videoPlayer', { static: false })
-  videoplayer!: ElementRef;
-  isPlay: boolean = false;
-  toggleVideo(event: any) {
-    this.videoplayer.nativeElement.play();
-  }
-  playPause() {
-    const myVideo: any = document.getElementById('my_video_1');
-    if (myVideo.paused) myVideo.play();
-    else myVideo.pause();
-  }
-
-  makeBig() {
-    const myVideo: any = document.getElementById('my_video_1');
-    myVideo.width = 560;
-  }
-
-  makeSmall() {
-    const myVideo: any = document.getElementById('my_video_1');
-    myVideo.width = 320;
-  }
-
-  makeNormal() {
-    const myVideo: any = document.getElementById('my_video_1');
-    myVideo.width = 420;
-  }
-
-  skip(value: any) {
-    let video: any = document.getElementById('my_video_1');
-    video.currentTime += value;
-  }
-
-  restart() {
-    let video: any = document.getElementById('my_video_1');
-    video.currentTime = 0;
-  }
+  //   @ViewChild('videoPlayer', { static: false })
+  //   videoplayer!: ElementRef;
+  //   isPlay: boolean = false;
+  //   toggleVideo(event: any) {
+  //     this.videoplayer.nativeElement.play();
+  //   }
+  //   playPause() {
+  //     const myVideo: any = document.getElementById('my_video_1');
+  //     if (myVideo.paused) myVideo.play();
+  //     else myVideo.pause();
+  //   }
+  //   makeBig() {
+  //     const myVideo: any = document.getElementById('my_video_1');
+  //     myVideo.width = 560;
+  //   }
+  //   makeSmall() {
+  //     const myVideo: any = document.getElementById('my_video_1');
+  //     myVideo.width = 320;
+  //   }
+  //   makeNormal() {
+  //     const myVideo: any = document.getElementById('my_video_1');
+  //     myVideo.width = 420;
+  //   }
+  //   skip(value: any) {
+  //     let video: any = document.getElementById('my_video_1');
+  //     video.currentTime += value;
+  //   }
+  //   restart() {
+  //     let video: any = document.getElementById('my_video_1');
+  //     video.currentTime = 0;
+  //   }
 }
