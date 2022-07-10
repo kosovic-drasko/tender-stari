d8ca4bcb5cfa6001653eaaad56671034cc8ca3b2
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-07-10 17:32:05 +0200

    init

--- src/main/webapp/app/entities/vrednovanje/vrednovanje.module.ts
+++ src/main/webapp/app/entities/vrednovanje/vrednovanje.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'app/shared/shared.module';
-import { VrednovanjeComponent } from './list/vrednovanje.component';
-import { VrednovanjeDetailComponent } from './detail/vrednovanje-detail.component';
+
 import { VrednovanjeRoutingModule } from './route/vrednovanje-routing.module';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { JhMaterialModule } from 'app/shared/jh-material.module';
 import { MatTableExporterModule } from 'mat-table-exporter';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { VrednovanjeComponent } from './list/vrednovanje.component';
 
 @NgModule({
-  imports: [SharedModule, VrednovanjeRoutingModule, MatPaginatorModule, JhMaterialModule, MatTableExporterModule, MatSortModule],
-  declarations: [VrednovanjeComponent, VrednovanjeDetailComponent],
+  imports: [
+    MatSidenavModule,
+    SharedModule,
+    VrednovanjeRoutingModule,
+    MatPaginatorModule,
+    JhMaterialModule,
+    MatTableExporterModule,
+    MatSortModule,
+  ],
+  declarations: [VrednovanjeComponent],
+  exports: [VrednovanjeComponent],
 })
 export class VrednovanjeModule {}
