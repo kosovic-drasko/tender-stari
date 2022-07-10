9c90a5bb82490843f6902fe7a03da099c25ba61c
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-07-10 18:50:43 +0200

    hh

--- src/main/webapp/app/entities/tenderi-home/tenderi-home.module.ts
+++ src/main/webapp/app/entities/tenderi-home/tenderi-home.module.ts
@@ -1,4 +1,3 @@
-import { BodovanjeComponent } from './../bodovanje/bodovanje.component';
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from 'app/shared/shared.module';
@@ -12,21 +11,13 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { HideMeDirective } from './hide-me.directive';
 import { PonudeModule } from '../ponude/ponude.module';
 import { SpecifikacijeModule } from '../specifikacije/specifikacije.module';
-import { VrednovanjeComponent } from '../vrednovanje/list/vrednovanje.component';
 import { DecimalPipe } from '@angular/common';
 import { PrvorangiraniComponent } from '../prvorangirani/list/prvorangirani.component';
 import { HvalePonudeComponent } from '../hvale-ponude/list/hvale-ponude.component';
 
 @NgModule({
   imports: [SharedModule, TenderiHomeRoutingModule, MatTabsModule, JhMaterialModule, SpecifikacijeModule, PonudeModule],
-  declarations: [
-    TenderiHomeComponent,
-    HideMeDirective,
-    VrednovanjeComponent,
-    PrvorangiraniComponent,
-    HvalePonudeComponent,
-    BodovanjeComponent,
-  ],
+  declarations: [TenderiHomeComponent, HideMeDirective, PrvorangiraniComponent, HvalePonudeComponent],
   providers: [DecimalPipe],
 })
 export class TenderiHomeModule {}
