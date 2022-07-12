16b851226836f8cce129122253703b105273e9af
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-07-12 20:44:25 +0200

    init

--- src/main/webapp/app/entities/tenderi-home/tenderi-home.module.ts
+++ src/main/webapp/app/entities/tenderi-home/tenderi-home.module.ts
@@ -1,11 +1,9 @@
 import { PrvorangiraniModule } from './../prvorangirani/prvorangirani.module';
 import { NgModule } from '@angular/core';
-
 import { SharedModule } from 'app/shared/shared.module';
 import { TenderiHomeComponent } from './list/tenderi-home.component';
 import { TenderiHomeRoutingModule } from './route/tenderi-home-routing.module';
 import { JhMaterialModule } from '../../shared/jh-material.module';
-
 import { MatTabsModule } from '@angular/material/tabs';
 import { HideMeDirective } from './hide-me.directive';
 import { PonudeModule } from '../ponude/ponude.module';
